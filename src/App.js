54bd013df120aa2780ac70d22e8975bd0614bee8
Abdi Mohamud <abdinajibmohamud@gmail.com>
2021-07-13 12:17:45 -0700

    added some deps, wrapped router across app, going to deploy ahead of time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import {Switch, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import PickATeam from './pages/PickATeam'
+import Team from './pages/Team'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/"><Home/></Route>
+        <Route path="/pick-a-team"><PickATeam/></Route>
+        <Route path="/team/:id"><Team/></Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/PickATeam.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PickATeam = () => {
+    return (
+        <div>
+            Pick your Team
+        </div>
+    )
+}
+
+export default PickATeam
