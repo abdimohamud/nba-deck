96636bb4793b67ae12b3db0702e2dd282b22a037
Abdi Mohamud <abdinajibmohamud@gmail.com>
2021-07-13 18:08:03 -0700

    added some of the ind team obj keys, wrapping up

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,38 +1,156 @@
 
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
 import { useParams } from 'react-router';
 import teams from '../data.json'
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { Typography } from '@material-ui/core';
+import clsx from 'clsx';
+// import Chart from './Chart';
+// import Deposits from './Deposits';
+// import Orders from './Orders';
+const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
   root: {
+    display: 'flex',
+  },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: 'none',
+  },
+  title: {
     flexGrow: 1,
   },
-  paper: {
-    height: 140,
-    width: 100,
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
-  control: {
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9),
+    },
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
     padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 302,
   },
 }));
-
 export default function Team() {
   const {id} = useParams();
   const [team, setTeam] = useState(null)
-  const [spacing, setSpacing] = React.useState(2);
+
   const classes = useStyles();
-  console.log(team)
- 
+  console.log(id)
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
   useEffect(()=>{
-    if(id){
-      let res = teams.filter(e => e.abv === id)[0]
+    if(!team){
+      let res = teams.filter(e => e.abv.toString() === id.toString())[0]
+      console.log(res)
       setTeam(res)
     }
-  }, [id])
-  return (
+  }, [team])
+  return team && (
     <Grid container className={classes.root} spacing={2} >
-     
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+        <Box pt={4}>
+          <img src={team.banner} alt={team.banner}/>
+          </Box>
+          <Grid container spacing={3}>
+            {/* Chart */}
+            <Grid item xs={12} md={8} lg={9}>
+              <Paper className={fixedHeightPaper}>
+                {/* <Chart /> */}
+                <React.Fragment>
+                <Typography component="h2" variant="h6" color="primary" gutterBottom>{team.name} ({team.abv})</Typography>
+     <Paper>Home</Paper>
+     {team.desc}
+        </React.Fragment>
+              </Paper>
+            </Grid>
+            {/* Recent Deposits */}
+            <Grid item xs={12} md={4} lg={3}>
+              <Paper className={fixedHeightPaper}>
+             <img   id ={team.name}
+                  alt={team.abv}
+                  src={team.badge} />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={4} lg={3}>
+              <Paper className={fixedHeightPaper}>
+             <img   id ={team.name}
+                  alt={team.jersey}
+                  src={team.jersey} />
+              </Paper>
+            </Grid>
+            {/* Recent Orders */}
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>
+                {/* <Orders /> */}
+                <i class="fab fa-youtube"></i>
+              </Paper>
+            </Grid>
+          </Grid>
+       
+        </Container>
+      </main>
   </Grid>
   );
 }
