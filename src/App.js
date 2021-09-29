9ba7bbb7516f096648c8c30d51634a3d031ddcd3
Abdi Mo <65041807+abdimohamud@users.noreply.github.com>
2021-09-29 12:02:03 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import PickATeam from './pages/PickATeam'
 import Team from './pages/Team'
+import Tos from './pages/Tos'
 function App() {
   const [favTeam, setFavTeam] = useState(null)
  
@@ -15,6 +16,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/"><Home/></Route>
+        <Route path="/tos"><Tos/></Route>
         <Route path="/pick-a-team"><PickATeam setFavTeam={setFavTeam} handleClick={handleClick} favTeam={favTeam}/></Route>
         <Route path="/team/:id"><Team favTeam={favTeam}/></Route>
       </Switch>
